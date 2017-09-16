6374adc2378d81b2d2813426109d28ea0854926e
fedjensa <jens.hedberg@medieinstitutet.se>
2017-09-16 15:09:01 +0200

    showing posts

--- includes/setup.php
+++ includes/setup.php
@@ -2,6 +2,7 @@
 
 function ju_setup_theme() {
     //add_theme_support( 'menus' ); not needed in newer versions of wp
+    add_theme_support( 'post-thumbnails' );
 
     register_nav_menu( 'primary', __('Primary Menu', 'wp') );
 }
