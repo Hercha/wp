a881b03a240327162e2c87e038e764564e8ce7af
fedjensa <jens.hedberg@medieinstitutet.se>
2017-09-20 23:07:05 +0200

    plugin progress

--- recipe/index.php
+++ recipe/index.php
@@ -13,14 +13,20 @@ if( !function_exists( 'add_action' ) ) {
 }
 
 // Setup
-
+define( 'RECIPE_PLUGIN_URL', __FILE__ );
 
 // Includes
 include( 'includes/activate.php' );
 include( 'includes/init.php' );
+include( 'includes/admin/init.php' );
+include( 'process/save-post.php' );
+include( 'process/filter-content.php' );
 
 // Hooks
 register_activation_hook( __FILE__, 'r_activate_plugin' );
 add_action( 'init', 'recipe_init' );
+add_action( 'admin_init', 'recipe_admin_init' );
+add_action( 'save_post_recipe', 'r_save_post_admin', 10, 3 );
+add_action( 'the_content', 'r_filter_recipe_content' );
 
 // Shortcodes
