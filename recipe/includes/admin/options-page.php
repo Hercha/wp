0d4fc47d39fb39bd90d3e325abdf8216050f8950
fedjensa <jens.hedberg@medieinstitutet.se>
2017-09-23 23:13:22 +0200

    recipe admin panel settings

--- recipe/includes/admin/options-page.php
+++ recipe/includes/admin/options-page.php
@@ -1,6 +1,8 @@
 <?php
 
 function r_plugin_opts_page() {
+    $recipe_opts = get_option( 'r_opts' );
+
     ?>
 
 <div class="wrap">
@@ -9,6 +11,13 @@ function r_plugin_opts_page() {
             <h3 class="panel-title"><?php _e('Recipe Settings', 'recipe' ); ?></h3>
         </div>
         <div class="panel-body">
+            <?php
+            
+            if( isset($_GET['status']) && $_GET['status'] == 1 ){
+                ?><div class="alert alert-success">Options updated successfully!</div><?php
+            }
+            
+            ?>
             <form method="POST" action="admin-post.php">
                 <input type="hidden" name="action" value="r_save_options">
                 <?php wp_nonce_field( 'r_options_verify' ); ?>
@@ -16,14 +25,14 @@ function r_plugin_opts_page() {
                     <label><?php _e('User login required for rating recipes', 'recipe'); ?></label>
                     <select class="form-control" name="r_rating_login_required">
                         <option value="1">No</option>
-                        <option value="2">Yes</option>
+                        <option value="2" <?= $recipe_opts['rating_login_required'] == 2 ? 'SELECTED' : ''; ?>>Yes</option>
                     </select>
                 </div>
                 <div class="form-group">
                     <label><?php _e('User login required for submitting recipes', 'recipe'); ?></label>
                     <select class="form-control" name="r_submission_login_required">
                         <option value="1">No</option>
-                        <option value="2">Yes</option>
+                        <option value="2" <?= $recipe_opts['recipe_submission_login_required'] == 2 ? 'SELECTED' : ''; ?>>Yes</option>
                     </select>
                 </div>
                 <div class="form-group">
@@ -32,6 +41,19 @@ function r_plugin_opts_page() {
             </form>
         </div>
     </div>
+
+    <hr>
+
+    <!--<form method="POST" action="options.php">
+        <?php
+
+            settings_fields( 'r_opts_groups' );
+            do_settings_sections( 'r_opts_sections' );
+            submit_button();
+
+        ?>
+    </form> -->
+
 </div>
 
     <?php
