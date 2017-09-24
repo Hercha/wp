98c9b5e9325455da62af6310e962542dbb76ea72
fedjensa <jens.hedberg@medieinstitutet.se>
2017-09-24 20:49:28 +0200

    buddypress cont

--- functions.php
+++ functions.php
@@ -11,6 +11,9 @@ include( get_template_directory() . '/includes/customizer/social.php' );
 include( get_template_directory() . '/includes/customizer/misc.php' );
 include( get_template_directory() . '/includes/customizer/enqueue.php' );
 include( get_template_directory() . '/includes/buddypress/cover-image.php' );
+include( get_template_directory() . '/includes/buddypress/profile-tabs.php' );
+include( get_template_directory() . '/includes/functions.php' );
+include( get_template_directory() . '/includes/buddypress/profile-posts.php' );
 
 // Hooks
 add_action( 'wp_enqueue_scripts', 'ju_enqueue' );
@@ -19,6 +22,8 @@ add_action( 'widgets_init', 'ju_widgets' );
 add_action( 'customize_register', 'ju_customize_register' );
 add_action( 'customize_preview_init', 'ju_customize_preview_init'  );
 add_filter( 'bp_before_xprofile_cover_image_settings_parse_args', 'ju_xprofile_cover_image' );
+add_filter( 'bp_before_before_groups_cover_image_settings_parse_args', 'ju_xprofile_cover_image' );
+add_action( 'bp_setup_nav', 'ju_buddypress_profile_tabs' );
 remove_action( 'shutdown', 'wp_ob_end_flush_all', 1 );// Temorary fix of ob_end_flush(): failed to send buffer of zlib output compression (1) problem
 
 // Shortcodes
