01b64aae2f6cd95fe7606ce8a5645c4efcf9a098
fedjensa <jens.hedberg@medieinstitutet.se>
2017-09-24 15:37:16 +0200

    buddypress start cont

--- functions.php
+++ functions.php
@@ -10,6 +10,7 @@ include( get_template_directory() . '/includes/theme-customizer.php' );
 include( get_template_directory() . '/includes/customizer/social.php' );
 include( get_template_directory() . '/includes/customizer/misc.php' );
 include( get_template_directory() . '/includes/customizer/enqueue.php' );
+include( get_template_directory() . '/includes/buddypress/cover-image.php' );
 
 // Hooks
 add_action( 'wp_enqueue_scripts', 'ju_enqueue' );
@@ -17,6 +18,7 @@ add_action( 'after_setup_theme', 'ju_setup_theme' );
 add_action( 'widgets_init', 'ju_widgets' );
 add_action( 'customize_register', 'ju_customize_register' );
 add_action( 'customize_preview_init', 'ju_customize_preview_init'  );
+add_filter( 'bp_before_xprofile_cover_image_settings_parse_args', 'ju_xprofile_cover_image' );
 remove_action( 'shutdown', 'wp_ob_end_flush_all', 1 );// Temorary fix of ob_end_flush(): failed to send buffer of zlib output compression (1) problem
 
 // Shortcodes
---
+++ includes/buddypress/cover-image.php
@@ -0,0 +1,8 @@
+<?php
+
+function ju_xprofile_cover_image( $settings = array() ) {
+    $settings['width']  = 1920;
+    $settings['height'] = 225;
+
+    return $settings;
+}
